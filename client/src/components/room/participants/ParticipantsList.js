e964e2d6b98e2999476e5437e18f3410641e0266
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2022-08-22 01:35:22 +0530

    room page basic layout and sidepanel ui done

---
+++ client/src/components/room/participants/ParticipantsList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ParticipantsList.module.css";
+
+const ParticipantsList = () => {
+  return <div className={styles.mainContainer}></div>;
+};
+
+export default ParticipantsList;
--- client/src/screens/RoomScreen/RoomScreen.js
+++ client/src/screens/RoomScreen/RoomScreen.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./RoomScreen.module.css";
+import close from "../../assets/icons/close.svg";
+import hamburger from "../../assets/icons/hamburger.svg";
+import SidePanel from "../../components/room/sidepanel/SidePanel";
 
 const RoomScreen = () => {
-  return <div className={styles.mainContainer}>RoomScreen</div>;
+  const [openSidePanel, setOpenSidePanel] = useState(true);
+
+  return (
+    <div className={styles.mainContainer}>
+      <div data-aos="fade-right" className={styles.mainPanelContainer}>
+        <img
+          className={styles.sidePanelToggle}
+          onClick={() => setOpenSidePanel(!openSidePanel)}
+          src={openSidePanel ? close : hamburger}
+          alt="spnl-icon"
+        />
+      </div>
+      {openSidePanel && (
+        <div data-aos="fade-right" className={styles.sidePanelContainer}>
+          <SidePanel />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RoomScreen;
