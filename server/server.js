6d72ced240d2bfcfb80c6e51482791209117b671
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2022-09-27 01:16:15 +0530

    socket.io connections done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 const twilio = require("twilio");
+//const { createNewRoomHandler, joinRoomHandler } = require("./handlers");
 
 const PORT = process.env.PORT || 5002;
 
@@ -34,6 +35,102 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"],
   },
 });
+
+io.on("connection", (socket) => {
+  console.log(`user connected ${socket.id}`);
+
+  socket.on("create-new-room", (data) => {
+    createNewRoomHandler(data, socket);
+  });
+
+  socket.on("join-room", (data) => {
+    joinRoomHandler(data, socket);
+  });
+
+  socket.on("disconnect", () => {
+    disconnectHandler(socket);
+  });
+});
+
+const createNewRoomHandler = (data, socket) => {
+  console.log(`Host is creating new room`, data);
+  const { identity } = data;
+  const roomId = uuidv4();
+
+  const newUser = {
+    identity,
+    id: uuidv4(),
+    socketId: socket.id,
+    roomId,
+  };
+
+  const newRoom = {
+    id: roomId,
+    connectedUsers: [newUser],
+  };
+
+  connectedUsers = [...connectedUsers, newUser];
+  rooms = [...rooms, newRoom];
+
+  socket.join(roomId);
+
+  socket.emit("room-id", { roomId });
+  socket.emit("room-update", { connectedUsers: newRoom.connectedUsers });
+};
+
+const joinRoomHandler = (data, socket) => {
+  const { identity, roomId } = data;
+
+  const newUser = {
+    identity,
+    id: uuidv4(),
+    socketId: socket.id,
+    roomId,
+    //onlyAudio,
+  };
+
+  // join room as user which just is trying to join room passing room id
+  const room = rooms.find((room) => room.id === roomId);
+  room.connectedUsers = [...room.connectedUsers, newUser];
+
+  // join socket.io room
+  socket.join(roomId);
+
+  // add new user to connected users array
+  connectedUsers = [...connectedUsers, newUser];
+
+  // emit to all users which are already in this room to prepare peer connection
+  room.connectedUsers.forEach((user) => {
+    if (user.socketId !== socket.id) {
+      const data = {
+        connUserSocketId: socket.id,
+      };
+
+      io.to(user.socketId).emit("conn-prepare", data);
+    }
+  });
+
+  io.to(roomId).emit("room-update", { connectedUsers: room.connectedUsers });
+};
+
+const disconnectHandler = (socket) => {
+  const user = connectedUsers.find((user) => user.socketId === socket.id);
+
+  if (user) {
+    const room = rooms.find((room) => room.id === user.roomId);
+    room.connectedUsers = room.connectedUsers.filter(
+      (user) => user.socketId !== socket.id
+    );
+    socket.leave(user.roomId);
+    if (room.connectedUsers.length > 0) {
+      io.to(room.id).emit("room-update", {
+        connectedUsers: room.connectedUsers,
+      });
+    } else {
+      rooms = rooms.filter((r) => r.id !== room.id);
+    }
+  }
+};
 
 server.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
